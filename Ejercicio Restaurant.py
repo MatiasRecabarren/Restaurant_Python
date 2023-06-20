14d85855358b72ee3e23e7f0d3300d2bc59cfa2d
Matías Recabarren <136729177+MatiasRecabarren@users.noreply.github.com>
2023-06-20 09:17:17 -0400

    Add files via upload

---
+++ Ejercicio Restaurant.py
@@ -0,0 +1,33 @@
+import numpy
+import os
+import time
+import msvcrt
+
+
+arreglo_restaurant = numpy.zeros((3,3), int)
+
+
+while True:
+    os.system("cls")
+    print ('''Menu Restaurant
+    1.Ver Restaurant
+    2.Reserva Mesa
+    3.Carta
+    4.Pagar
+    5.Cancelar
+    6.Salir
+    ''')
+    opcion = input("Ingrese una opción (1 a 6)")
+
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        pass
+    else:
+        break
